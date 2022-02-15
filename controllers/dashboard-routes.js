c778b5b44e528dae4f5cee604c59bd881f427019
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-15 12:03:08 -0600

    added partials and other views

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -11,18 +11,7 @@ router.get('/', withAuth, (req, res) => {
       //use ID from the session
       user_id: req.session.user_id,
     },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'
-        ),
-        'vote_count',
-      ],
-    ],
+    attributes: ['id', 'title', 'contents', 'created_at'],
     include: [
       {
         model: Comment,
@@ -51,18 +40,7 @@ router.get('/', withAuth, (req, res) => {
 
 router.get('/edit/:id', withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT(*) FROM VOTE WHERE post.id = vote.post_id)'
-        ),
-        'vote_count',
-      ],
-    ],
+    attributes: ['id', 'title', 'contents', 'created_at'],
     include: [
       {
         model: Comment,
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,18 +5,7 @@ const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
   console.log('=======================');
   Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'
-        ),
-        'vote_count',
-      ],
-    ],
+    attributes: ['id', 'title', 'contents', 'created_at'],
     include: [
       {
         model: Comment,
@@ -51,18 +40,7 @@ router.get('/post/:id', (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
-        ),
-        'vote_count',
-      ],
-    ],
+    attributes: ['id', 'title', 'contents', 'created_at'],
     include: [
       {
         model: Comment,
