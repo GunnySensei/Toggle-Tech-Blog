45c76aa20c3cee997ac5f7b726bd0da74b18d29b
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-16 12:01:27 -0600

    fixed commenting features/css

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,30 +1,6 @@
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 async function loginFormHandler(event) {
   event.preventDefault();
+  console.log(event);
 
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
@@ -47,9 +23,6 @@ async function loginFormHandler(event) {
   }
 }
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,5 +1,33 @@
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 async function signupRedirect() {
   window.redirect('/signup');
 }
 
 document.querySelector('#signup-btn').addEventListener('click', signupRedirect);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
